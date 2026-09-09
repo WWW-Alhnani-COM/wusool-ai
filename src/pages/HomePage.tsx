8aa57902493ded761a367991b43895dc2e603346
Mohammad Alhnani <145880506+WWW-Alhnani-COM@users.noreply.github.com>
2026-09-09 23:00:14 +0300

    Remove import for SectorsShowcase component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import { ScrollSequence } from "@/components/hero/ScrollSequence";
 import { ProblemSection } from "@/components/sections/ProblemSection";
 import { SolutionPath } from "@/components/sections/SolutionPath";
 import { ServicesGrid } from "@/components/sections/ServicesGrid";
-import { SectorsShowcase } from "@/components/sections/SectorsShowcase";
 import { ProcessTimeline } from "@/components/sections/ProcessTimeline";
 import { ValueSection } from "@/components/sections/ValueSection";
 import { FinalCTA } from "@/components/sections/FinalCTA";
